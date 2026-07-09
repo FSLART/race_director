0f17d3936de41e31055366fd59e97b687fee7cf3
André Lopes <andreglopes2005@gmail.com>
2026-07-09 23:14:33 +0100

    macro

--- src/race_director.cpp
+++ src/race_director.cpp
@@ -50,8 +50,8 @@ RaceDirector::RaceDirector() : Node("race_director"){
     //     this->perception_timestamp_timer = this->create_wall_timer(std::chrono::seconds(2), std::bind(&RaceDirector::request_perception_timestamp, this));
     // }
 
-    this->start_bag_recording_client = this->create_client<rosbag2_interfaces::srv::Resume>("/rosbag2_recorder/resume");
-    this->stop_bag_recording_client = this->create_client<rosbag2_interfaces::srv::Stop>("/rosbag2_recorder/stop");
+    this->start_bag_recording_client = this->create_client<rosbag2_interfaces::srv::Resume>(SERVICE_START_BAG_RECORDING);
+    this->stop_bag_recording_client = this->create_client<rosbag2_interfaces::srv::Stop>(SERVICE_STOP_BAG_RECORDING);
 
     this->handbook_msgs_timer = this->create_wall_timer(std::chrono::duration<double>(0.1), std::bind(&RaceDirector::send_handbook_msgs, this));
     // this->steering_timestamp_timer = this->create_wall_timer(std::chrono::seconds(1), std::bind(&RaceDirector::check_steering_timestamp, this));
@@ -92,7 +92,7 @@ void RaceDirector::acu_callback(const lart_msgs::msg::Acu::SharedPtr msg) {
         }
     }else{
         if (this->bag_recording){
-            this->stop_bag_recording();
+            this->schedule_bag_stop();
         }
     }
 
